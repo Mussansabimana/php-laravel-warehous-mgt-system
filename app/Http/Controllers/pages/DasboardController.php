8b8ae8065676eb3d364c79e6a927b1a03123644f
Mussa Sabimana <nsamussa1@gmail.com>
2025-05-15 12:18:44 +0200

    new commit of error fixing

--- app/Http/Controllers/pages/DasboardController.php
+++ app/Http/Controllers/pages/DasboardController.php
@@ -6,19 +6,28 @@ use App\Http\Controllers\Controller;
 use App\Models\products\Furniture;
 use App\Models\products\Export;
 use App\Models\products\Import;
+use Carbon\Carbon;
 
 class DasboardController extends Controller
 {
     public function index()
     {
+
+
+        // Furniture count for today where quantity != 0
         $furnitureCount = Furniture::with('imports')
             ->where('quantity', '!=', 0)
+            ->whereDate('created_at', Carbon::today())
             ->orderBy('created_at', 'desc')
             ->count();
-        $importCount = Import::count();
-        $exportCount = Export::count();
+
+        // Import count for today
+        $importCount = Import::whereDate('created_at', Carbon::today())->count();
+
+        // Export count for today
+        $exportCount = Export::whereDate('created_at', Carbon::today())->count();
+
 
         return view('pages.dashboard', compact('furnitureCount', 'importCount', 'exportCount'));
     }
 }
-
